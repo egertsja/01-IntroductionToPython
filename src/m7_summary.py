3eabcc708f10789c8dc1c33f2b3dad4b8c8c133d
egertsja <jae242000@gmail.com>
2019-03-04 11:28:29 -0500

    DONE

--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,16 +2,16 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Jonah Egertson.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Write code that accomplishes the following (and ONLY the following),
 #   in the order listed:
 #
@@ -43,3 +43,24 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+tortoise = rg.SimpleTurtle()
+tortoise.pen = rg.Pen('blue',10)
+
+tortoise.left(90)
+tortoise.forward(200)
+tortoise.pen_up()
+
+tortoise.right(90)
+tortoise.forward(100)
+tortoise.right(90)
+tortoise.forward(240)
+
+tortoise.pen_down()
+tortoise.pen = rg.Pen('green',10)
+tortoise.forward(150)
+
+window.close_on_mouse_click()
